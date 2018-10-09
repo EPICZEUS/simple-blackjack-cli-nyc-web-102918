651cade030d1f91c92ede98305009eb930fac331
Sam B <s77broz@aol.com>
2018-10-09 19:52:11 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -7,15 +7,15 @@ def deal_card
 end
 
 def display_card_total(total)
-  # code #display_card_total here
+  puts total
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
 def end_game
